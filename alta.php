123f1e9f38e95d95a398476f3c31e5cf4e376ef9
root <root@web.lan>
2022-02-28 17:42:42 -0600

    Agrego archivos php y form

---
+++ alta.php
@@ -0,0 +1,11 @@
+<?php
+	//var_dump($_POST);
+	$nombre = $_POST["nombre"];
+	$apaterno = $_POST["apaterno"];
+	$amaterno = $_POST["amaterno"];
+	$correoe = $_POST["correoe"];
+
+	echo "Hola " . $nombre . " " . $apaterno . " " . "$amaterno";
+	echo "<br/>"
+	echo "Tu correo es: " . $correoe
+?>
