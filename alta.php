34b3f8d42a196bd2f07a0cb9756ffb9e405b3536
root <root@web.lan>
2022-02-28 18:14:26 -0600

    correcciones

--- alta.php
+++ alta.php
@@ -1,11 +1,11 @@
 <?php
 	//var_dump($_POST);
-	$nombre = $_POST["nombre"];
-	$apaterno = $_POST["apaterno"];
-	$amaterno = $_POST["amaterno"];
-	$correoe = $_POST["correoe"];
+	$nombre= $_POST['nombre'];
+	$apaterno= $_POST['apaterno'];
+	$amaterno= $_POST['amaterno'];
+	$correoe= $_POST['correoe'];
 
-	echo "Hola " . $nombre . " " . $apaterno . " " . "$amaterno";
-	echo "<br/>"
-	echo "Tu correo es: " . $correoe
+	echo "Hola " . $nombre . " " . $apaterno . " " . $amaterno;
+	echo "<br/>";
+	echo "Tu correo es: " . $correoe;
 ?>
